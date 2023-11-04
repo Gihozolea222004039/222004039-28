709df622b463a797baa559082b8798642d0ecb92
Gihozolea222004039 <149673743+Gihozolea222004039@users.noreply.github.com>
2023-11-04 18:43:34 +0200

    Update break2_forloop.java
    adding comments

--- break2_forloop.java
+++ break2_forloop.java
@@ -4,11 +4,12 @@ public class break2_forloop
     {
       int number=10;
       do{
-        System.out.println(number);
-        number++;
-        if(number==16)break;
+        System.out.println(number);    //PRINT OUT THE CURRENT NUMBER
+        number++;                // INCREAMENT CURRENT NUMBER BY 1
+        if(number==16)break;    //STOP / BREAK THE LOOP WHENEVER number BECOME 16
       } 
-      while(number<=20); 
+      while(number<=20);     //CONDITION TO BE FOLLOWED BY COMPILER, 
+                            //IF TRUE IT LOOP AGAIN TILL THE CONDITION BECOME FALSE
     }
     } 
     
